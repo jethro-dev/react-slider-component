7983fcb7f1e438847c76ea4ca46a347b49b3e7e6
Jethro Au <supergalong@gmail.com>
2021-09-23 00:36:53 +0100

    update gitignore

---
+++ build/precache-manifest.e6e732c5b2be22a7a97a8cde42ac73a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9578ee023d9e1e22f039b94088f2ff6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbd05a4cff0b111fc8aa",
+    "url": "/static/css/main.fa3e7353.chunk.css"
+  },
+  {
+    "revision": "e1c4e21724a78a9e8542",
+    "url": "/static/js/2.2c3ffba9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c3ffba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbd05a4cff0b111fc8aa",
+    "url": "/static/js/main.ce7b51b2.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
